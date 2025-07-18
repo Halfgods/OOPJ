f64cd9c1fd9eb66ba05a4fccc971cb05184a5e89
Halfgods <justinroy9702@gmail.com>
2025-07-18 07:24:50 +0000

    Add Transpose.java for matrix transpose functionality

---
+++ Transpose.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Transpose {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number of rows and columns of the matrix:");
+        int rows = s.nextInt();
+        int cols = s.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+        System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = s.nextInt();
+            }
+        }
+
+        System.out.println("The original matrix is:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("The transposed matrix is:");
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                System.out.print(matrix[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
