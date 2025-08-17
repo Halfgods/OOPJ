08d9d3c0337731237aab9122e6a6fa0bc8207ea4
Halfgods <justinroy9702@gmail.com>
2025-08-17 14:57:42 +0000

    inheritance

---
+++ inherit.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Vehicle {
+    Scanner sc = new Scanner(System.in);
+	int Vehicle_no;
+	void f1 {
+	System.out.println("The Vehicle with" + Vehicle_no + "Is created.");
+	}
+}
+class Car extends Vehicle{
+    Scanner sc = new Scanner(System.in);
+	int Engine_no;
+	void f2 {
+	System.out.println("The Car with" + Engine_no + "Is created.");
+	}
+}
+class Bike extends Vehicle{
+    Scanner sc = new Scanner(System.in);
+	int Registration_no;
+	void f3 {
+	System.out.println("The Bike with" + Registration_no + "Is created.");
+	}
+}
+public class Demo{
+	public static void main(String[]args){
+
+	}
+}
